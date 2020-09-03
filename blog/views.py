9fadb7fb128828f28ea7e619ea5df9a41c684b53
Eduardo Ortiz <eduortiz.g@gmail.com>
2020-09-03 06:23:49 +0000

    Agregando Primer Model and View

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def posts(request):
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'blog/posts.html', {'posts' : posts})
+
+    ## COMO SE TRADUCE LO DE ARRIBA EN SQL?
+    ## SELECT * FROM Post WHERE publish_date <= DATETIME.NOW() ORDER BY publish_date
