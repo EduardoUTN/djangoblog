b76990afa3684eb46a4d5a0ad7653b6385ff2398
Eduardo Ortiz <eduortiz.g@gmail.com>
2020-09-15 19:14:23 +0000

    Agregado conexion MySQL y creada carpeta apps

--- informatorio/settings/base.py
+++ informatorio/settings/base.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    'apps.blog'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'informatorio.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'informatorio',
+       'USER': 'bloguser',
+       'PASSWORD': 'password',
+       'HOST': 'localhost',
+       'PORT': '3306',
+       },
 }
+
 
 
 # Password validation
