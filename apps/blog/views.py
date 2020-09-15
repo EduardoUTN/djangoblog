f4992e57ca74515c9e17458bd42e770b601ddc26
Eduardo Ortiz <eduortiz.g@gmail.com>
2020-09-15 19:33:30 +0000

    Agregada Blog Home y refactor de directorio templates

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+# Homepage
+def home(request):
+    return render(request, 'home.html', {})
+
 # Create your views here.
 def posts(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
-    return render(request, 'blog/posts.html', {'posts' : posts})
+    return render(request, 'posts.html', {'posts' : posts})
 
     ## COMO SE TRADUCE LO DE ARRIBA EN SQL?
     ## SELECT * FROM Post WHERE publish_date <= DATETIME.NOW() ORDER BY publish_date
